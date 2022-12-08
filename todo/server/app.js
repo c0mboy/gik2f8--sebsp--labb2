26ce11bcc0e9565834556e81c46bd93fb08b0466
c0mboy <90767788+c0mboy@users.noreply.github.com>
2022-12-08 09:50:44 +0100

    Post function done!

--- todo/server/app.js
+++ todo/server/app.js
@@ -11,7 +11,7 @@ app
   .use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Method", "*");
+    res.header("Access-Control-Allow-Methods", "*");
 
     next();
   });
@@ -25,4 +25,31 @@ app.get("/task", async (req, res) => {
   }
 });
 
+app.post("/task", async (req, res) => {
+  try {
+    const task = req.body;
+
+    const listBuffer = await fs.readFile("./tasks.json");
+    const currentTasks = JSON.parse(listBuffer);
+    // sätter id
+    let maxTaskId = 1;
+    if (currentTasks && currentTasks.length > 0) {
+      maxTaskId = currentTasks.reduce(
+        (maxId, currentElement) =>
+          currentElement.id > maxId ? currentElement.id : maxId,
+        maxTaskId
+      );
+    }
+    //variabel för att att använda flera gångermed +1 på id och spread operator
+    const newTask = { id: maxTaskId + 1, ...task };
+    const newList = currentTasks ? [...currentTasks, newTask] : [newTask];
+    console.log(newList);
+    //Skriver det i filen och gör om det till en sträng
+    await fs.writeFile("./tasks.json", JSON.stringify(newList));
+    //res.send([task, ...currentTasks]); // Spread operator allt som finns i currentasks kommer läggas som tilläg i task
+    res.send(newTask);
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
+});
 app.listen(PORT, () => console.log("Server running on http://localhost:5000"));
