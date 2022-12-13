11aed8743295cf4fff789604ccfc9ff98feb0cc9
c0mboy <90767788+c0mboy@users.noreply.github.com>
2022-12-13 18:10:40 +0100

    else in switch, and rendertask() html

--- todo/client/script.js
+++ todo/client/script.js
@@ -11,6 +11,8 @@ todoForm.dueDate.addEventListener("blur", (e) => validateField(e.target));
 
 todoForm.addEventListener("submit", onSubmit);
 
+const todoListElement = document.getElementById("todoList"); //UL listan från HTML
+
 let titleValid = true;
 let descriptionValid = true;
 let dueDateValid = true;
@@ -32,6 +34,8 @@ function validateField(field) {
         titleValid = false;
         validationMessage =
           "Fältet 'Titel' får inte innehålla mer än 100 tecken. ";
+      } else {
+        titleValid = true;
       }
       break;
     }
@@ -40,6 +44,8 @@ function validateField(field) {
         descriptionValid = false;
         validationMessage =
           "Fältet 'Beskrivning' får inte innehålla mer än 100 tecken. ";
+      } else {
+        descriptionValid = true;
       }
       break;
     }
@@ -47,6 +53,8 @@ function validateField(field) {
       if (value.length == 0) {
         dueDateValid = false;
         validationMessage = "Fältet 'Slutförd senast' är obligatorisk. ";
+      } else {
+        dueDateValid = true;
       }
       break;
     }
@@ -78,17 +86,48 @@ function onSubmit(e) {
     //skickar tasks till api creat som heter data sedan gör om det till JSON sträng. then för att ta emot return från api som är ett promise
     api.create(task).then((task) => {
       if (task) {
-        render();
+        renderList();
       }
     });
   }
 }
 
 /* Ta emot ny task och lägga till den i listan */
-function render() {
+function renderList() {
   console.log("rendering");
   api.getAll().then((tasks) => {
-    console.log(tasks);
     /* Generera lista av uppgifter */
+    if (tasks && tasks.length > 0) {
+      todoListElement.innerHTML = ""; //tar bort html i Ul börja från 0
+
+      tasks.forEach((task) => {
+        todoListElement.insertAdjacentHTML("beforeend", renderTask(task)); //skall lägga in html i slutet från renderTask()
+      });
+    }
   });
 }
+
+function renderTask({ id, title, description, dueDate }) {
+  //Kommer att plocka ut just de egenskaper man vill ha se mina val
+  //Html CODE
+  let html = `
+    <li class="select-none mt-2 py-2 border-b border-sky-300">
+     <div class="flex items-center">
+      <h3 class="mb-3 flex-1 text-l font-bold text-purple-500 uppercase">${title}</h3>
+      <div>
+       <span>${dueDate}</span>
+       <button class="inline-block text-xs rounded-md bg-pink-400 hover:bg-pink-500 py-1 px-3 rounded-md ml-2">Delete</button>
+      </div>
+     </div>`;
+  // Om descriptons skriv med kommer det att visas eller inte
+  description &&
+    (html += `
+        <p class="ml-8 mt-2 text-xs italic">${description}</p> 
+        `);
+  html += `
+    </li>`;
+
+  return html; //returnerar ut html koden
+}
+
+renderList(); //visar All uppgifter direkt vid laddning av sidan
